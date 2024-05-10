784c04901934772e27ff9029798bb1e9d488cd7a
Maria <mativaclub@gmail.com>
2024-05-10 11:02:23 +0300

    Creating method of new array with condition of size less then 3 elements

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,36 @@ public class Main {
         String input = scanner.nextLine();
         String[] originalArray = input.split(" ");
 
+        String[] newArray = filterShortStrings(originalArray);
 
+        System.out.println("Новый массив строк:");
+        for (String str : newArray) {
+            System.out.print(str + " ");
+        }
+    }
+
+    public static String[] filterShortStrings(String[] array) {
+        int count = 0;
+        // Определяем количество строк длиной не более 3 символов
+        for (String str : array) {
+            if (str.length() <= 3) {
+                count++;
+            }
+        }
+
+        // Создаем новый массив нужного размера
+        String[] resultArray = new String[count];
+
+        // Заполняем новый массив строками длиной не более 3 символов
+        int index = 0;
+        for (String str : array) {
+            if (str.length() <= 3) {
+                resultArray[index] = str;
+                index++;
+            }
+        }
+
+        return resultArray;
     }
 
 }
